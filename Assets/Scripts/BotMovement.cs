ec714d9c360b1a4fe1d7a734e875f6c6247d0dbf
Daniel Ebenezer <danielebenezer.developer@gmail.com>
2020-09-02 21:40:55 +0530

    Coin Collecting BOT AI

---
+++ Assets/Scripts/BotMovement.cs
@@ -0,0 +1,75 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class BotMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    
+    int shortest;
+    private int coinCount=0;
+
+    public Transform spawn;
+    public List<Transform> Coins = new List<Transform>();
+
+    public Transform coinParent;
+
+    public Text coinText;
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        CheckCoins();
+    }
+
+    private void Update()
+    {
+        if(Coins.Count==0)
+        {
+            agent.SetDestination(spawn.position);
+        }
+        coinText.text = "Coins Collected:" + coinCount + " ";
+    }
+
+    void CheckClosest(List<Transform> _coins)
+    {
+        if (_coins.Count != 0)
+        {
+            shortest = 0;
+
+            for (int j = 0; j < _coins.Count; j++)
+            {
+                float a = Vector3.Distance(transform.position, Coins[shortest].transform.position);
+                float b = Vector3.Distance(transform.position, Coins[j].transform.position);
+                if (a > b)
+                {
+                    shortest = j;
+                }
+            }
+
+            agent.SetDestination(Coins[shortest].position);
+        }
+    }
+
+    public void CheckCoins()
+    {
+        //when coin is instantiated it needs to be a child of coinParent
+
+        for (int i = 0; i < coinParent.transform.childCount; i++)
+        {
+            Coins.Add(coinParent.transform.GetChild(i));
+        }
+        CheckClosest(Coins);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            Coins.Remove(other.gameObject.transform);
+            CheckClosest(Coins);
+            other.gameObject.SetActive(false);
+            coinCount++;
+        }
+    }
+}
---
+++ Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NavMeshBaker : MonoBehaviour
+{
+
+    [SerializeField]
+    NavMeshSurface[] navMeshSurfaces;
+    
+    //to bake navmesh to surface during runtime
+    private void Awake()
+    {
+        for(int i=0;i<navMeshSurfaces.Length; i++)
+        {
+            navMeshSurfaces[i].BuildNavMesh();
+        }
+    }
+}
