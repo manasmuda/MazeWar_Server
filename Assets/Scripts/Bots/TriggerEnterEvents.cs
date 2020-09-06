9a8cd7a2356b4ac3e8b7942bdea79b865ab89a7d
manas <manasgupta435@gmail.com>
2020-09-06 19:21:45 +0530

    changes1

---
+++ Assets/Scripts/Bots/TriggerEnterEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TriggerEnterEvents : MonoBehaviour
+{
+    public string checkTag;
+
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag(checkTag))
+        {
+            triggerEvent.Invoke();
+        }
+    }
+}
